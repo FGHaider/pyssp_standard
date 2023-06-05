c2c2b2fdf6f9e8d3a5f55b54f2c9a9240822dc21
fhaider <5920+fhaider@users.noreply.gits-15.sys.kth.se>
2023-06-05 13:43:11 +0200

    Updates

--- ssb.py
+++ ssb.py
@@ -1,8 +1,15 @@
-from transformation_types import Transformation
+from parameter_types import ParameterType
+from ssm import Annotations
 from utils import SSPStandard
 import xml.etree.cElementTree as ET
 from pathlib import Path, PosixPath
 from typing import TypedDict, List
+
+
+class DictionaryEntry(TypedDict):
+    name: str
+    type_entry: ParameterType
+    annotations: Annotations
 
 
 class SSB(SSPStandard):
@@ -20,10 +27,16 @@ class SSB(SSPStandard):
             file_path = Path(file_path)
         self.file_path = file_path
 
+        self.version = None
+        self.top_level_meta_data = None
+
         self.__tree = None
         self.__root = None
 
         self.__annotations = []
+        self.__dictionary_entry = List[DictionaryEntry]
+        self.__enumerations = []
+        self.__units = []
 
         if mode == 'r' or mode == 'a':
             self.__read__()
@@ -34,11 +47,19 @@ class SSB(SSPStandard):
         self.__tree = ET.parse(self.file_path)
         self.__root = self.__tree.getroot()
 
+        self.version = self.__root.get('version')
+        dictionary_entries = self.__root.findall('ssb:DictionaryEntry', self.namespaces)
+        for entry in dictionary_entries:
+            name = entry.get('name')
+            self.__dictionary_entry.append(DictionaryEntry(name=name, type_entry=ParameterType(),
+                                                           annotations=Annotations()))
+
     def __write__(self):
         pass
 
     def __save__(self):
-        pass
+        tree = ET.ElementTree(self.__root)
+        tree.write(self.file_path, encoding='utf-8', xml_declaration=True)
 
     def __check_compliance__(self):
         pass
--- ssd.py
+++ ssd.py
@@ -5,13 +5,28 @@ from pathlib import Path, PosixPath
 from typing import TypedDict, List
 
 
+class System(SSPStandard):
+
+    def __init__(self, system_element: ET.Element):
+
+        self.name = None
+
+        self.connectors = []
+        self.parameter_bindings = []
+
+        self.elements = []
+        self.connections = []
+        self.signal_dictionaries = []
+        self.annotations = []
+
+
 class SSD(SSPStandard):
 
     def __enter__(self):
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.__mode in ['w', 'a']:
+        if self.__mode == 'a':
             self.__save__()
 
     def __init__(self, file_path, mode='r'):
@@ -23,19 +38,26 @@ class SSD(SSPStandard):
         self.__tree = None
         self.__root = None
 
+        self.name = None
+        self.version = None
+
+        self.system = None
+        self.__enumerations = []
+        self.__units = []
+        self.defaultExperiment = None
         self.__annotations = []
 
         if mode == 'r' or mode == 'a':
             self.__read__()
-        elif mode == 'w':
-            self.__write__()
+        else:
+            raise Exception('Only read and append mode is supported for SSD files')
 
     def __read__(self):
         self.__tree = ET.parse(self.file_path)
         self.__root = self.__tree.getroot()
 
-    def __write__(self):
-        pass
+        self.name = self.__root.get('name')
+        self.version = self.__root.get('version')
 
     def __save__(self):
         pass
