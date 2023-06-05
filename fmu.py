7f664344b28779e1a6385051170e9adc1bb2a8c6
fhaider <5920+fhaider@users.noreply.gits-15.sys.kth.se>
2023-06-05 13:14:37 +0200

    Added schemas and continued improvements to ssv

---
+++ fmu.py
@@ -0,0 +1,89 @@
+import shutil
+import tempfile
+import zipfile
+from pathlib import Path, PosixPath
+import xml.etree.cElementTree as ET
+from typing import TypedDict
+
+
+class ScalarVariable(TypedDict):
+    name: str
+    description: str
+    causality: str
+    variability: str
+    type_declaration: dict
+
+
+class FMU:
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        shutil.rmtree(self.temp_dir)
+
+    def __init__(self, file_path, mode='r'):
+        """
+        This class allows for peeking into a FMU file, it by no means covers the entirety of the FMI standard and should
+         primarily be considered as a debugging tool for looking of model input, outputs and parameters.
+        :param file_path: file path to the target FMU
+        :param mode: Mode to open file in, currently only read mode is available
+        """
+        if type(file_path) is not PosixPath:
+            file_path = Path(file_path)
+        self.temp_dir = tempfile.mkdtemp()
+        self.file_path = file_path
+        self.model_description_file = None
+
+        self.__parameters = []
+        self.__inputs = []
+        self.__outputs = []
+        self.__other = []
+
+        if mode != 'r':
+            raise Exception('Only read mode is possible with FMUs.')
+
+        self.__read__()
+
+    def __read__(self):
+        with zipfile.ZipFile(self.file_path, 'r') as zip_ref:
+            zip_ref.extractall(self.temp_dir)
+
+        self.model_description_file = list(Path(self.temp_dir).glob('modelDescription.xml'))[0]
+        tree = ET.parse(self.model_description_file)
+        root = tree.getroot()
+
+        model_variables = root.findall('ModelVariables')[0]
+        scalar_variables = model_variables.findall('ScalarVariable')
+        for scalar in scalar_variables:
+            name = scalar.get('name')
+            description = scalar.get('description')
+            causality = scalar.get('causality')
+            variability = scalar.get('variability')
+            #type_declaration = list(scalar.iter())
+            scalar_variable = ScalarVariable(name=name, description=description, variability=variability,
+                                             type_declaration={}, causality=causality)
+            if causality == 'parameter':
+                self.__parameters.append(scalar_variable)
+            elif causality == 'input':
+                self.__inputs.append(scalar_variable)
+            elif causality == 'output':
+                self.__outputs.append(scalar_variable)
+            else:
+                self.__other.append(scalar_variable)
+
+    @property
+    def parameters(self):
+        return self.__parameters
+
+    @property
+    def outputs(self):
+        return self.__outputs
+
+    @property
+    def inputs(self):
+        return self.__inputs
+
+    @property
+    def other(self):
+        return self.__other
--- ssp.py
+++ ssp.py
@@ -17,6 +17,12 @@ class SSP(SSPStandard):
         self.temp_dir = tempfile.mkdtemp()
         self.file_path = file_path
 
+        self.__ssd = None
+        self.__ssv = None
+        self.__ssm = None
+        self.__ssb = None
+        self.__fmu = None
+
     def __read__(self):
 
         with zipfile.ZipFile(self.file_path, 'r') as zip_ref:
@@ -27,3 +33,23 @@ class SSP(SSPStandard):
 
     def __save__(self):
         pass
+
+    @property
+    def ssd(self):
+        return self.__ssd
+
+    @property
+    def ssv(self):
+        return self.__ssv
+
+    @property
+    def ssm(self):
+        return self.__ssm
+
+    @property
+    def ssb(self):
+        return self.__ssb
+
+    @property
+    def fmu(self):
+        return self.__fmu
