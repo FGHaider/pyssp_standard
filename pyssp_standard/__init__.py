2a289493836ab882ffc3157610832e83d7ec0dc7
FGHaider <fredrikhaider@outlook.com>
2023-08-12 14:55:52 +0200

    __init__.py update

--- pyssp_standard/__init__.py
+++ pyssp_standard/__init__.py
@@ -1,6 +1,8 @@
-from pyssp_standard.ssd import SSD
+from pyssp_standard.ssd import SSD, Connection
 from pyssp_standard.ssb import SSB
 from pyssp_standard.ssv import SSV
 from pyssp_standard.ssm import SSM
 from pyssp_standard.fmu import FMU
 from pyssp_standard.ssp import SSP
+from pyssp_standard.transformation_types import Transformation
+from pyssp_standard.common_content_ssc import Annotations, Annotation
