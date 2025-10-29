726ac24ce9cba5ca28805a89a463422bdfcd3ba1
Axel Matstoms <axel.matstoms@gmail.com>
2025-10-29 14:03:57 +0100

    Update Units to use dict
    Using a dict ensures name uniqueness, which is required by the SSP
    standard, and allows merging different unit sets more easily.

--- pyssp_standard/unit.py
+++ pyssp_standard/unit.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, asdict
+
 from lxml import etree as ET
 from lxml.etree import QName
 
@@ -27,7 +28,9 @@ class BaseUnit:
                     try:
                         value = field_type(value)
                     except (TypeError, ValueError):
-                        raise ValueError(f"Invalid value type for {field_name}. Expected {field_type}.")
+                        raise ValueError(
+                            f"Invalid value type for {field_name}. Expected {field_type}."
+                        )
                 setattr(self, field_name, value)
 
     def to_dict(self):
@@ -35,7 +38,6 @@ class BaseUnit:
 
 
 class Unit(SSPElement, ModelicaStandard):
-
     def __init__(self, unit, base_unit: BaseUnit = None):
         self.name = None
         self.base_unit = None
@@ -49,22 +51,18 @@ class Unit(SSPElement, ModelicaStandard):
     def to_element(self, namespace="ssc"):
         ns = "" if not namespace else f"{{{self.namespaces[namespace]}}}"
 
-        unit_entry = ET.Element(f"{ns}Unit", attrib={'name': self.name})
+        unit_entry = ET.Element(f"{ns}Unit", attrib={"name": self.name})
 
         if namespace == "ssc" and self.base_unit is None:
             self.base_unit = BaseUnit({})  # In FMI BaseUnit is optional, in SSP it is required
 
         if self.base_unit is not None:
-            ET.SubElement(
-                unit_entry,
-                f"{ns}BaseUnit",
-                **self.base_unit.to_dict()
-            )
+            ET.SubElement(unit_entry, f"{ns}BaseUnit", **self.base_unit.to_dict())
 
         return unit_entry
 
     def from_element(self, element):
-        self.name = element.attrib.get('name')
+        self.name = element.attrib.get("name")
         ns = QName(element.tag).namespace
         tag_name = "ssc:BaseUnit" if ns is not None else "BaseUnit"
 
@@ -74,9 +72,8 @@ class Unit(SSPElement, ModelicaStandard):
 
 
 class Units(ModelicaStandard):
-
     def __init__(self, element: ET.Element = None):
-        self.units = []
+        self.units = {}
 
         if element is not None:
             namespace = "ssc:"
@@ -84,13 +81,33 @@ class Units(ModelicaStandard):
                 namespace = ""
 
             units = element.findall(f"{namespace}Unit", self.namespaces)
-            for unit in units:
-                self.units.append(Unit(unit, namespace))
+            for unit_elem in units:
+                unit = Unit(unit_elem, namespace)
+                self.units[unit.name] = unit
 
     def add_unit(self, unit: Unit):
-        self.units.append(unit)
+        """Add unit to units listing.
 
-    def element(self, parent_type='ssc'):
+        If unit already exists but with a different definition a ValueError is raised.
+        Otherwise, update listing to include the passed unit, overriding base unit if
+        not already in listing.
+        """
+        if unit.name in self:
+            own = self.units[unit.name]
+
+            # Same name, different base unit
+            if (
+                own.base_unit is not None
+                and unit.base_unit is not None
+                and own.base_unit != unit.base_unit
+            ):
+                raise ValueError(f"Unit {unit.name} already exists with different definition")
+
+            own.base_unit = own.base_unit or unit.base_unit
+        else:
+            self.units[unit.name] = unit
+
+    def element(self, parent_type="ssc"):
         if parent_type == "fmi":
             elem_name = "UnitDefinitions"
             child_ns = None
@@ -99,15 +116,21 @@ class Units(ModelicaStandard):
             child_ns = "ssc"
 
         root = ET.Element(elem_name)
-        root.extend(unit.to_element(namespace=child_ns) for unit in self.units)
+        root.extend(unit.to_element(namespace=child_ns) for unit in self.units.values())
 
         return root
 
     def __len__(self):
         return len(self.units)
 
-    def __getitem__(self, idx):
-        return self.units[idx]
+    def __iter__(self):
+        return iter(self.units.values())
+
+    def __getitem__(self, key):
+        return self.units[key]
+
+    def __contains__(self, key):
+        return key in self.units
 
     def is_empty(self):
         return True if len(self.units) == 0 else False
